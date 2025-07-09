e9b1b0228d7b52c9f5fbf0c2041664fe2640dcad
chirag <sharmachirag242004@gmail.com>
2025-07-09 21:43:16 +0530

    cors implemented

--- auth part1/index.js
+++ auth part1/index.js
@@ -1,7 +1,12 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import cors from 'cors';
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(urlencoded({extended: true}));
+app.use(cors({
+    origin: 'localhost: 3000'
+}))
 
 app.get('/', (req, res) => {
     res.send("hello world");
