dc45b43d2319a566a524732d52113caac7cd1130
chirag <sharmachirag242004@gmail.com>
2025-07-10 14:19:58 +0530

    initial commit

--- auth part1/index.js
+++ auth part1/index.js
@@ -2,6 +2,7 @@ import express, { urlencoded } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { db } from './utils/db.js';
+import User from './models/user-model.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
@@ -18,6 +19,18 @@ app.use(cors({
 app.get('/', (req, res) => {
     res.send("hello world");
 });
+
+app.post('/', (req, res) => {
+    const {firstName} = req.body;
+    User.insertOne(firstName).then(() => {
+        console.log("user created successfully")
+    }).catch((err) => {
+        console.log('something went wrong', err);
+    });
+    res.send({
+        msg: "user created"
+    })
+})
 
 db();
 
