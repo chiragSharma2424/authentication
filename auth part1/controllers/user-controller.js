eebc6de9360655219f74150941241f6d02035ac9
chirag <sharmachirag242004@gmail.com>
2025-07-10 22:25:29 +0530

    initial commit

--- auth part1/controllers/user-controller.js
+++ auth part1/controllers/user-controller.js
@@ -1,3 +1,44 @@
+import User from '../models/user-model.js';
+import crypto from 'crypto'
+
 export const register = async (req, res) => {
-    res.send("hello user register");
+    const { name, email, password } = req.body;
+
+    if(!name || !email || password) {
+        return res.status(400).json({
+            success: false,
+            msg: "all fields are required"
+        })
+    }
+
+    if(password.length < 6) {
+        return res.status(400).json({
+            success: false,
+            msg: "password must be atleast 6 characters"
+        })
+    }
+
+
+    try {
+        // checking user already exists or not in database
+        const existingUser = await User.findOne({
+            email
+        });
+
+        if(existingUser) {
+            return res.status(400).json({
+                success: false,
+                msg: "user already exist in database"
+            })
+        }
+
+        // now user verification token 
+        
+    } catch(err) {
+        console.log(err);
+        res.json({
+            success: false,
+            msg: "internal server error"
+        })
+    }
 }
--- auth part1/models/user-model.js
+++ auth part1/models/user-model.js
@@ -11,4 +11,4 @@ const userSchema = new mongoose.Schema({
 }, {timestamps: true});
 
 const User = mongoose.model("User", userSchema);
-export default User; 
+export default User;
