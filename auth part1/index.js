2ab8a69a193651da87f4c5ead982ddd232468c24
chirag <sharmachirag242004@gmail.com>
2025-07-10 15:10:39 +0530

    basic schema and controller done

--- auth part1/index.js
+++ auth part1/index.js
@@ -2,7 +2,7 @@ import express, { urlencoded } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { db } from './utils/db.js';
-import User from './models/user-model.js';
+import userRoutes from './routes/user-routes.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
@@ -16,21 +16,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.get('/', (req, res) => {
-    res.send("hello world");
-});
-
-app.post('/', (req, res) => {
-    const {firstName} = req.body;
-    User.insertOne(firstName).then(() => {
-        console.log("user created successfully")
-    }).catch((err) => {
-        console.log('something went wrong', err);
-    });
-    res.send({
-        msg: "user created"
-    })
-})
+app.use('/api/v1/users', userRoutes);
 
 db();
 
