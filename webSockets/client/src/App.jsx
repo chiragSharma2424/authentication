07c1e6108627c35d405fbfb89a1ad74efa88f570
chirag <sharmachirag242004@gmail.com>
2025-07-12 23:24:01 +0530

    basic practice

--- webSockets/client/src/App.jsx
+++ webSockets/client/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {io} from 'socket.io-client'
 
 function App() {
   const socket = io('http://localhost:3000');
+
+  useEffect(() => {
+    socket.on("connect", () => {});
+    console.log("Connected", socket.id);
+  }, []);
+
   return (
     <div>
-      
+      App
     </div>
   )
 }
