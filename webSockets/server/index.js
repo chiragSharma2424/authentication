1b7117deec142c2ec686847428caede6f0f0fc90
chirag <sharmachirag242004@gmail.com>
2025-07-12 19:26:01 +0530

    web socket server setup

--- webSockets/server/index.js
+++ webSockets/server/index.js
@@ -1,14 +1,33 @@
 import express from 'express';
-import { Server } from 'socket.io'
+import { Server } from 'socket.io';
+import cors from 'cors';
+import { createServer } from 'http';
 const app = express();
 const port = 3000;
-const server = new Server(app);
-const io = new Server(server);
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+        credentials: true,
+    },
+});
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
 
 app.get('/', (req, res) => {
     res.send("hello world");
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("user connected");
+    console.log("Id ", socket.id);
+});
+
+server.listen(port, () => {
     console.log(`server is running on port ${port}`)
 });
